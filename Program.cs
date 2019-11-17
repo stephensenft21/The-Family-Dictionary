8e514fd43833061ed54995157270f2dfb016a53f
stephensenft21 <stephensenft21@gmail.com>
2019-11-16 18:26:33 -0600

    finished family dictionary

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TheFamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+myFamily.Add("sister", new Dictionary<string, string>(){
+    {"name", "Krista"},
+    {"eyeColor", "blue"},
+    {"age", "42"}
+});
+myFamily.Add("brother", new Dictionary<string, string>(){
+    {"name", "Michael"},
+    {"eyeColor", "blue"},
+    {"age", "37"}
+});
+myFamily.Add("father", new Dictionary<string, string>(){
+    {"name", "fred"},
+    {"eyeColor", "blue"},
+    {"age", "65"}
+});
+myFamily.Add("mother", new Dictionary<string, string>(){
+    {"name", "mary"},
+    {"eyeColor", "blue"},
+    {"age", "63"},
+    
+});
+
+
+foreach(KeyValuePair<string, Dictionary<string, string>> familyMember in myFamily)
+{
+    Console.WriteLine($"{familyMember.Value["name"]} is my {familyMember.Key} and her eyes {familyMember.Value["eyeColor"]}. she is also  {familyMember.Value["age"]} YEARS OLD!!");
+}
+//I would liek to be able to switch the between genders on the statements
+
+        }
+
+        
+    }
+}
+    
